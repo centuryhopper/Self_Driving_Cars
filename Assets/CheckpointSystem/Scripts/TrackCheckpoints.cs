b50adc8ca0a9cccabe0ae86f778504497d75ebd1
Leo Zhang <leotheasianlion@gmail.com>
2022-08-07 21:55:21 -0400

    basic features and mechanics are working

--- Assets/CheckpointSystem/Scripts/TrackCheckpoints.cs
+++ Assets/CheckpointSystem/Scripts/TrackCheckpoints.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// main class that will hold all the CheckpointSingle objects
 /// </summary>
-public class TrackCheckpoints : MonoBehaviour {
-
+public class TrackCheckpoints : MonoBehaviour
+{
     public event EventHandler OnPlayerCorrectCheckpoint;
     public event EventHandler OnPlayerWrongCheckpoint;
 
@@ -16,11 +15,21 @@ public class TrackCheckpoints : MonoBehaviour {
     private List<CheckpointSingle> checkpointSingleList;
     private List<int> nextCheckpointSingleIndexList;
 
-    private void Awake() {
-        Transform checkpointsTransform = transform.Find("Checkpoints");
+    private void Awake()
+    {
+        string objNameToFind = "CheckPoints";
+        Transform checkpointsTransform = GameObject.Find(objNameToFind)?.transform;
+
+        if (checkpointsTransform is null)
+        {
+            UnityEngine.Debug.LogError($"couldn't find the gameobject with the name: {objNameToFind}");
+            return;
+        }
 
         checkpointSingleList = new List<CheckpointSingle>();
-        foreach (Transform checkpointSingleTransform in checkpointsTransform) {
+        foreach (Transform checkpointSingleTransform in checkpointsTransform)
+        {
+            print(checkpointSingleTransform.name);
             CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
 
             checkpointSingle.SetTrackCheckpoints(this);
@@ -29,14 +38,28 @@ public class TrackCheckpoints : MonoBehaviour {
         }
 
         nextCheckpointSingleIndexList = new List<int>();
-        foreach (Transform carTransform in carTransformList) {
+
+        if (carTransformList is null || carTransformList.Count == 0)
+        {
+            var cars = GameObject.Find("Cars")?.transform;
+            foreach (var car in cars)
+            {
+                carTransformList.Add(car as Transform);
+            }
+        }
+        foreach (Transform carTransform in carTransformList)
+        {
             nextCheckpointSingleIndexList.Add(0);
         }
     }
 
-    public void CarThroughCheckpoint(CheckpointSingle checkpointSingle, Transform carTransform) {
+    public void CarThroughCheckpoint(CheckpointSingle checkpointSingle, Transform carTransform)
+    {
+        print($"carTransformList.IndexOf(carTransform): {carTransformList.IndexOf(carTransform)}");
+        print($"nextCheckpointSingleIndexList.Count: {nextCheckpointSingleIndexList.Count}");
         int nextCheckpointSingleIndex = nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)];
-        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex) {
+        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex)
+        {
             // Correct checkpoint
             Debug.Log("Correct");
             CheckpointSingle correctCheckpointSingle = checkpointSingleList[nextCheckpointSingleIndex];
@@ -45,7 +68,9 @@ public class TrackCheckpoints : MonoBehaviour {
             nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)]
                 = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
             OnPlayerCorrectCheckpoint?.Invoke(this, EventArgs.Empty);
-        } else {
+        }
+        else
+        {
             // Wrong checkpoint
             Debug.Log("Wrong");
             OnPlayerWrongCheckpoint?.Invoke(this, EventArgs.Empty);
